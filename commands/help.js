b120cfd3693d4adaeb5ef8e645516d31a82f0703
Kragborg <Emil.eriksen@gmail.com>
2021-04-19 11:24:33 +0200

    da

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'help',
     description: "this is a ping command! ",
     execute(message, args, user){
-        message.channel.send(`Hej ${user.username}! - Tusind tak for at bruge MatBot. \n Matbot er udviklet specielt til at hjælpe unge med matematetiske problemer \n Skriv "!commands" for at finde alle de tilgængelige commands, som MatBot tilbyder \n God fornøjelse med at bruge Matbot og dets funktioner`);
+        message.channel.send(`Hello ${user.username}! - Thanks for choosing MathBot. \n Mathbot is made to help ypung people with mathematical problems and equations \n Write "!commands" to find the commads, that MathBot can use \n Enjoy using MathBot and tis commands`);
     }
 }
