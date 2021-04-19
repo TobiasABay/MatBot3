d221434378fc7c5a1dbdebbedc5875b6d3e1209f
Tobias Bay <hammer100@hotmail.dk>
2021-04-19 11:27:57 +0200

    Update percent.js

--- commands/percent.js
+++ commands/percent.js
@@ -6,6 +6,15 @@ module.exports = {
 
         if (args[0] == 'help') {
             return message.channel.send(`If you wanna calculate a percentage of two numbers, then write the command like this "!p [first numer] [second number]" - example; "!p 20 80" ${message.author}!`);
-        };
-    }
+        }
+
+        if (!args[0] || !args[1]) {
+            return message.channel.send(`Ukendt værdi, for mere hjælp skriv "!bmi help", ${user.username}!`);
+        }
+        else{
+            result = args[0] / args(1) * 100
+            result_floor = Math.floor(result);
+        }
+        message.channel.send(`\nResult: ${args[0]} / (${args[1]} * 100 = ${result_floor}`
+    )}
 }
