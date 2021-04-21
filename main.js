348bde1e43a4e7d22297ef9d4ea4873cd20547b7
Tobias Bay <hammer100@hotmail.dk>
2021-04-21 08:57:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ client.on('ready', function (){
 client.on("guildCreate", guild => {
     const channels = guild.channels.cache.filter(channel => channel.type == "text");
 
-    channels.first().send("Thank you for inviting me!").catch(e => console.log(e));
+    channels.first().send("TAk for at vælge mig! \n Hvis du ønsker at vide mere om, hvad jeg kan gøre, så skriv følgende command: \n !help \n \n God fornøjelse").catch(e => console.log(e));
 });
 
 client.on('message', message => {
