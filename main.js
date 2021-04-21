7918e339e99d449bb950768f8c092b58bfe598a9
Tobias Bay <hammer100@hotmail.dk>
2021-04-21 09:16:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -15,15 +15,14 @@ for(const file of commandFiles){ //Bruger for loop til at loade filerne ind
 
 client.on('ready', function (){
     console.log(`Logged in as ${bot.user.tag}!`);
-    const channel = guild.channels.cache.find(channel => channel.type === 'text');
-    channel.send("THANK YOU for choosing me! \n  If you want to know more about what I can do, type the following command:\n !help\n \n God fornøjelse")
     
 });
 
 client.on("guildCreate", guild => {
     const channels = guild.channels.cache.filter(channel => channel.type == "text");
 
-    channels.first().send("Tak for at vælge mig! \n Hvis du ønsker at vide mere om, hvad jeg kan gøre, så skriv følgende command: \n !help \n \n God fornøjelse").catch(e => console.log(e));
+    channel.send("THANK YOU for choosing me! \n  If you want to know more about what I can do, type the following command:\n !help\n \n God fornøjelse")
+    
 });
 
 client.on('message', message => {
