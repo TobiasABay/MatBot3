3f2b09ea4d47de4d95659f80600243dfdac9022d
Tobias Bay <hammer100@hotmail.dk>
2021-04-21 13:40:40 +0200

    Update areal.js

--- commands/areal.js
+++ commands/areal.js
@@ -1,12 +1,17 @@
 module.exports = {
     name: 'area',
     description: "this is a ping command! ",
-    execute(message, args, user, command){
+    execute(message, args){
         let result, result_floor;
 
-        if (args[0] == 'help' || command === "area" || message == "area") {
+        if (!args[0]) {
+            return message.channel.send(`type: "!area help" for more help`);
+        }
+
+        if (args[0] == 'help' ||) {
             return message.channel.send(`If you wanna get the area of a given figure, then there gotta be space between the numbers like this example "!area triangle 3 5", "!area square 5 5" eller "!area pentagon 5" ${message.author}!`);
         }
+        
 
         if (args[0] == 'triangle' || args[0] == 'Triangle') {
             if (!args[0] || !args[1] || !args[2] ) {
