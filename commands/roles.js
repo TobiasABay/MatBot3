10c08706c0c531d09ca352eb3a0d1ec6687a20e3
Tobias_bay <45756799+sealbay@users.noreply.github.com>
2021-04-23 10:14:08 +0200

    Update roles.js

--- commands/roles.js
+++ commands/roles.js
@@ -46,7 +46,28 @@ module.exports = {
                 }
              
         });
+         
+        client.on('messageReactionRemove', async (reaction, user) => {
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
              
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.name === yellowTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(MatB);
+                }
+                if (reaction.emoji.name === greenTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(MatC);
+                }
+                if (reaction.emoji.name === redTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(MatA);
+                }
+                } else {
+                    return;
+                }
+             
+        });
     }
              
 }
