f7b8e3da466289d42d17bcc054e00ba75adbee1b
Tobias_bay <45756799+sealbay@users.noreply.github.com>
2021-04-23 09:07:39 +0200

    Update roles.js

--- commands/roles.js
+++ commands/roles.js
@@ -2,9 +2,8 @@ module.exports = {
     name: 'roles',
     description: "this is a ping command! ",
     execute(message, args, user){
-            message.channel.send("test")
+            message.react('🟢');
             message.react('🟡');
-	        message.react('🟢');
         	message.react('🔴');
         
     }
